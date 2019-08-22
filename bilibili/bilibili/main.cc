844e83974dd679bba88eff304123cbb5d9453d35
Caoyuewin <849807784@qq.com>
2019-08-22 23:09:32 +0800

    bilibili笔试

---
+++ bilibili/bilibili/main.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main() {
+//	string s;
+//	while (getline(cin, s)) {
+//		reverse(s.begin(), s.end());
+//		string tmp;
+//		int j = 0;
+//		for (int i = 0; i < s.size(); ++i) {
+//			if (s[i] == ' ') {
+//				reverse(s.begin() + j, s.begin() + i);
+//				j = i;
+//			}
+//		}
+//		//反转最后一个单词
+//		reverse(s.begin() + j, s.end());
+//		cout << s << endl;
+//	}
+//}
+
+//输入一串整数，每两个整数之间用逗号隔开，将这些整数拼接起来，输出拼接出的最小的数
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	string s;
+	while (cin >> s) {
+		int i = 0;
+		while (i < s.size()) {
+			
+		}
+	}
+	return 0;
+}
