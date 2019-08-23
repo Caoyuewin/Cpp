f84ab157b1b47aa775bed44b49d07f1fedee8568
Caoyuewin <849807784@qq.com>
2019-08-23 21:43:24 +0800

    笔试

---
+++ 完美世界/完美世界/No2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	//n为题数,score接收每个题的份数，time接收每个题花费的时间，total_time为考试总时间
+	string n;
+	string score;
+	string time;
+	string total_time;
+	int max_socre = 0;
+	while (getline(cin, n)) {
+		getline(cin, score);
+		getline(cin, time);
+		getline(cin, total_time);
+
+		//输入完成
+
+	}
+}
